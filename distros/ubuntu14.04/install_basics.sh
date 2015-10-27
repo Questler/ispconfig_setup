0f80bb2ea72d225f2a9cf160daa7137ecf5f2ca1
Questler <>
2015-10-27 23:22:20 +0100

    Added ubuntu 14.04

---
+++ distros/ubuntu14.04/install_basics.sh
@@ -0,0 +1,28 @@
+#---------------------------------------------------------------------
+# Function: InstallBasics
+#    Install basic packages
+#---------------------------------------------------------------------
+InstallBasics() {
+  echo -n "Updating apt and upgrading currently installed packages... "
+  echo "deb http://us-west-2.ec2.archive.ubuntu.com/ubuntu/ trusty main" > /etc/apt/sources.list
+  echo "deb-src http://us-west-2.ec2.archive.ubuntu.com/ubuntu/ trusty-updates main" >> /etc/apt/sources.list
+  echo "deb-src http://us-west-2.ec2.archive.ubuntu.com/ubuntu/ trusty universe" >> /etc/apt/sources.list
+  echo "deb-src http://us-west-2.ec2.archive.ubuntu.com/ubuntu/ trusty-updates universe" >> /etc/apt/sources.list
+  echo "deb-src http://us-west-2.ec2.archive.ubuntu.com/ubuntu/ trusty multiverse" >> /etc/apt/sources.list
+  echo "deb-src http://us-west-2.ec2.archive.ubuntu.com/ubuntu/ trusty-updates multiverse" >> /etc/apt/sources.list
+  echo "deb-src http://us-west-2.ec2.archive.ubuntu.com/ubuntu/ trusty-backports main restricted universe multiverse" >> /etc/apt/sources.list
+  echo "deb-src http://security.ubuntu.com/ubuntu trusty-security main" >> /etc/apt/sources.list
+  echo "deb-src http://security.ubuntu.com/ubuntu trusty-security universe" >> /etc/apt/sources.list
+
+  apt-get -qq update > /dev/null 2>&1
+  apt-get -qqy upgrade > /dev/null 2>&1
+  echo -e "[${green}DONE${NC}]\n"
+
+  echo "Installing basic packages... "
+  apt-get -y install ssh openssh-server vim-nox php5-cli ntp ntpdate debconf-utils binutils sudo git lsb-release > /dev/null 2>&1
+
+  echo "dash dash/sh boolean false" | debconf-set-selections
+  dpkg-reconfigure -f noninteractive dash > /dev/null 2>&1
+  echo -n "Reconfigure dash... "
+  echo -e "[${green}DONE${NC}]\n"
+}
